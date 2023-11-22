8abfefec8f8be10d71b2ff4cd010eac0f73a5d59
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-11-22 11:50:43 +0000

    Add test for add(), pass

--- lib/toy_storage.py
+++ lib/toy_storage.py
@@ -1,3 +1,6 @@
 class ToyStorage():
     def __init__(self):
         self.toy_list = []
+
+    def add(self, toy):
+        self.toy_list.append(toy)
--- tests/test_toy_storage.py
+++ tests/test_toy_storage.py
@@ -8,3 +8,15 @@ def test_instantiates_with_toy_list():
     actual = toy_storage.toy_list
     expected = []
     assert actual == expected
+
+"""
+Given I add a toy to the toy_list
+I want to see the toy in the toy_list
+"""
+def test_add_new_toy_to_toy_list():
+    toy_storage = ToyStorage()
+    toy_storage.add('teddy')
+
+    actual = toy_storage.toy_list
+    expected = ['teddy']
+    assert actual == expected
